977483616605d2eded7a7a6616a24a19ad08ffa3
Carlos Sousa <147412733+casousavng@users.noreply.github.com>
2024-11-27 14:48:48 +0000

    correçao charset

--- backoffice/controllers/db.php
+++ backoffice/controllers/db.php
@@ -1,12 +1,12 @@
 <?php
-// Configurações de conexão com o banco de dados Oracle
+// Configurações de conexão com a base de dados Oracle
 $host = 'localhost';
 $port = '1521';
 $sid = 'xe';
 $username = 'c##biblioteca';
 $password = 'senha123';
 
-$dsn = "oci:dbname=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=$host)(PORT=$port))(CONNECT_DATA=(SID=$sid)))";
+$dsn = "oci:dbname=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=$host)(PORT=$port))(CONNECT_DATA=(SID=$sid)));charset=UTF8";
 
 try {
     // Conexão com o banco de dados Oracle usando PDO
